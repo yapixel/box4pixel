d1ee9b29d2def54d2f00706adda1ad1e3577d91a
debiansid <debiansid@gmail.com>
2025-03-14 10:54:31 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -25,12 +25,12 @@ fi
 
 ui_print "- 正在释放新文件"
 unzip -o "$ZIPFILE" 'box/*' -d $unzip_path >&2
-unzip -j -o "$ZIPFILE" 'box4magisk_service.sh' -d /data/adb/service.d >&2
+unzip -j -o "$ZIPFILE" 'box4pixel_service.sh' -d /data/adb/service.d >&2
 unzip -j -o "$ZIPFILE" 'uninstall.sh' -d $MODPATH >&2
 unzip -j -o "$ZIPFILE" "module.prop" -d $MODPATH >&2
 #mkdir -p /data/adb/box/run/
 ui_print "- 正在设置权限"
-set_perm /data/adb/service.d/box4magisk_service.sh 0 0 0755
+set_perm /data/adb/service.d/box4pixel_service.sh 0 0 0755
 set_perm $MODPATH/uninstall.sh 0 0 0755
 set_perm_recursive $MODPATH 0 0 0755 0644
 set_perm_recursive $unzip_path/box 0 0 0755 0755
