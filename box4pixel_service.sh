0b96b81d98e0fa81134693de68a98ddca48f4128
debiansid <debiansid@gmail.com>
2025-03-19 12:46:40 +0800

    Update box4pixel_service.sh

--- box4pixel_service.sh
+++ box4pixel_service.sh
@@ -1,8 +1,16 @@
-#!/system/bin/sh
+#!/sbin/sh
+
+module_dir="/data/adb/modules/box4"
+
+[ -n "$(magisk -v | grep lite)" ] && module_dir=/data/adb/lite_modules/box4
+
+scripts_dir="/data/adb/box/scripts"
 
 (
-    until [ $(getprop sys.boot_completed) -eq 1 ]; do
-        sleep 5
-    done
-    /data/adb/box/scripts/start.sh
-) &
+until [ $(getprop sys.boot_completed) -eq 1 ] ; do
+  sleep 3
+done
+${scripts_dir}/start.sh
+)&
+
+inotifyd ${scripts_dir}/box.inotify ${module_dir} > /dev/null 2>&1 &
